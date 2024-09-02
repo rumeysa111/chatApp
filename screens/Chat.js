d55c5ad8a7c909c3346672680ecb303b46fc4009
rumeysa111 <rumeysasemiz11@gmail.com>
2024-09-02 21:39:45 +0300

    chat.js file updated

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,17 +1,60 @@
 import { useRoute } from "@react-navigation/native";
-import React ,{useEffect}from "react";
-import { Text } from "react-native";
+import React ,{useEffect,useState}from "react";
+import { Text,View } from "react-native";
 import firebase from "firebase/compat/app";
+import { GiftedChat } from "react-native-gifted-chat";
 
-const Chat =()=>{
-    const route=useRoute()
-    useEffect(()=>{
-        firebase.firestore().doc('chats/'+route.params.chatId)
-    })
+const Chat = () => {
+  const route = useRoute();
 
+  const [messages, setMessages] = useState([]);
 
-    return(
-        <Text>hi,Chat</Text>
-    )
-}
-export default Chat; 
+  const [uid, setUID] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    return firebase.auth().onAuthStateChanged((user) => {
+      setUID(user?.uid);
+      setName(user?.displayName);
+    });
+  }, []);
+
+  useEffect(() => {
+    return firebase
+      .firestore()
+      .doc("chats/" + route.params.chatId)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.data()?.messages ?? []);
+      });
+  }, [route.params.chatId]);
+
+  const onSend = (m = []) => {
+    firebase
+      .firestore()
+      .doc("chats/" + route.params.chatId)
+      .set(
+        {
+          messages: GiftedChat.append(messages, m),
+        },
+        { merge: true }
+      );
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <GiftedChat
+        messages={messages.map((x) => ({
+          ...x,
+          createdAt: x.createdAt?.toDate(),
+        }))}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: uid,
+          name: name,
+        }}
+      />
+    </View>
+  );
+};
+
+export default Chat;
